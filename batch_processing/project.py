0eff8ad45ac3b50833cf203100ee3ee3df5f57e1
Yalda Zarnegarnia <y.zarnegar@gmail.com>
2020-02-03 06:02:54 +0000

    get coordinates

--- batch_processing/project.py
+++ batch_processing/project.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import json
 import os
 import datetime                                                                                                                                                   
 import xml
@@ -39,6 +39,7 @@ def getxmlelement(xml_root,  path):
 
         return res
 
+
 def read_time(input_str, fmt="%Y-%m-%dT%H:%M:%S.%f"):
     dt = datetime.datetime.strptime(input_str, fmt)
     return dt
@@ -47,19 +48,30 @@ def read_time(input_str, fmt="%Y-%m-%dT%H:%M:%S.%f"):
 class BurstDataFrame:
 
     def __init__(self):
-      self.df = pd.DataFrame(columns=['Burst_ID','Track_Number','Pass_Direction','Node_Time','Burst_Start','Ground_Coordinates'])
+      self.df = pd.DataFrame(columns=['Node_Time','Burst_Start','Time_Difference','Track_Number','Burst_ID','Pass_Direction','Ground_Coordinate'])
+    
 
+    def getCoordinates(self, zipname):
+        zf = zipfile.ZipFile(zipname, 'r')
+
+        tiffpath = os.path.join('*SAFE','measurement', 's1a-iw1-slc*tiff')
+        match = fnmatch.filter(zf.namelist(), tiffpath)
+        zf.close()
+
+        tiffname = os.path.join('/vsizip/' + zipname, match[0])
+        cmd = "gdalinfo -json {} >> info.json".format(tiffname)
+        print(cmd)
+        os.system(cmd)
+        with open("info.json", 'r') as fid:
+           info = json.load(fid)
+
+
+        df_coordinates = pd.DataFrame(info['gcps']['gcpList'])
+        print(df_coordinates) 
+        os.system('rm info.json')
 
     def update(self, zipname):
         zf = zipfile.ZipFile(zipname, 'r')
-
-        #extract track number
-        #xmlpath = os.path.join('*SAFE','manifest.xml')
-        #match = fnmatch.filter(zf.namelist(), xmlpath)
-        #xmlstr = zf.read(match[0])
-        #xml_root = ET.fromstring(xmlstr)
-        
-
         xmlpath = os.path.join('*SAFE','annotation', 's1a-iw1-slc*xml')
         match = fnmatch.filter(zf.namelist(), xmlpath)
         xmlstr = zf.read(match[0])
@@ -70,14 +82,16 @@ class BurstDataFrame:
         numBursts = getxmlattr(xml_root, 'swathTiming/burstList', 'count')
         burstList = getxmlelement(xml_root, 'swathTiming/burstList')
         passtype=getxmlvalue(xml_root, 'generalAnnotation/productInformation/pass')
+        orbitnumber = int(getxmlvalue(xml_root, 'adsHeader/absoluteOrbitNumber'))
+        trackNumber = (orbitnumber-73)%175 + 1
 
         for index, burst in enumerate(list(burstList)):
             sensingStart = burst.find('azimuthTime').text
-            dt=read_time(sensingStart)-read_time(ascNodeTime)
-   
+            dt = read_time(sensingStart)-read_time(ascNodeTime)
+            burstID = str(trackNumber) + str(dt.seconds)
+            
             # check if self.df has this dt for this track. If not append it
-            self.df = self.df.append({'Burst_ID':dt,'Track_Number':100,'Pass_Direction':passtype,'Node_Time':ascNodeTime,'Burst_Start':sensingStart,'Ground_Coordinates':0}, ignore_index=True) 
-     
+            self.df = self.df.append({'Node_Time':ascNodeTime,'Burst_Start':sensingStart,'Time_Difference':dt, 'Track_Number':trackNumber,'Burst_ID':burstID, 'Pass_Direction':passtype,'Ground_Coordinate':0}, ignore_index=True)    
     
         zf.close()
   
@@ -95,9 +109,17 @@ if __name__ == "__main__":
 
     dfObj = BurstDataFrame()
     for ff in frames:
+        print(ff)
         dfObj.update(ff)
+        dfObj.getCoordinates(ff)
 
     print(dfObj.df)
+
+    
+
+
+
+
 
         #Download the zip file to S3 bucket
         #df extract_metadata(zipname, df)
